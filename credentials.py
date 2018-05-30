7152c69ec56ecf5254ddf575a581ff9b8aa2ae7f
Peter Seibel <seibelp@dnc.org>
2018-05-29 21:09:31 -0700

    Tidying.

--- credentials.py
+++ credentials.py
@@ -2,6 +2,9 @@
 
 from oauth2client import file
 
-scopes = 'https://www.googleapis.com/auth/calendar'
+scopes = [
+    'https://www.googleapis.com/auth/calendar',
+    'https://www.googleapis.com/auth/admin.directory.resource.calendar.readonly'
+]
 store  = file.Storage('credentials.json')
 creds  = store.get()
