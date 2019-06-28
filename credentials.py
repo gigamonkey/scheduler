183ee8e0872b89d8f667125d906ae4c4fb7e03cb
Peter Seibel <seibelp@dnc.org>
2019-06-28 15:37:31 -0700

    Adding Pipfile and reformatting.

--- credentials.py
+++ credentials.py
@@ -3,8 +3,8 @@
 from oauth2client import file
 
 scopes = [
-    'https://www.googleapis.com/auth/calendar',
-    'https://www.googleapis.com/auth/admin.directory.resource.calendar.readonly'
+    "https://www.googleapis.com/auth/calendar",
+    "https://www.googleapis.com/auth/admin.directory.resource.calendar.readonly",
 ]
-store  = file.Storage('credentials.json')
-creds  = store.get()
+store = file.Storage("credentials.json")
+creds = store.get()
