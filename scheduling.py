3b4814eef102f73b31b286d018de33db61641b62
Peter Seibel <seibelp@dnc.org>
2019-06-30 16:06:17 -0700

    Rename Item -> Meeting.

--- scheduling.py
+++ scheduling.py
@@ -1,7 +1,7 @@
 """
 Library for contraint-propagation-based scheduling.
 
-Schedules are made up of Items which are scheduled into one or more
+Schedules are made up of Meetings which are scheduled into one or more
 Slots and each have one or more Participants.
 
 We find schedules where all the items are put into Slots such that no
@@ -13,7 +13,7 @@ calendar or a more abstract notion (e.g. 60 minutes every Monday at
 could also be modeled as Participants as they often are in calendaring
 systems.)
 
-Items have criteria that constrain when they can be scheduled. This
+Meetings have criteria that constrain when they can be scheduled. This
 may be as simple as the duration--any free time will do. Or there
 could be other constraints such as happening on a certain day of the
 week or before a certain time.
@@ -22,7 +22,7 @@ Participants can also have constraints which further constrain when a
 meeting can be. For instance, if each Participant has a set of working
 hours, then a meeting can only be scheduled in the intersection of all
 the participants working hours. And Participants can only be scheduled
-in one Item per Slot.
+in one Meeting per Slot.
 """
 
 import dataclasses
@@ -32,10 +32,9 @@ from typing import Callable, Iterator, List, Optional, Set
 Predicate = Callable[["Slot"], bool]
 
 
-@dataclass(frozen=True)
 class Slot:
     """
-    A time that a thing can be scheduled. Items with overlapping
+    A time that a thing can be scheduled. Meetings with overlapping
     participants cannot be scheduled in slots that overlap.
     """
 
@@ -47,7 +46,7 @@ class Slot:
 @dataclass(frozen=True)
 class Participant:
     """
-    A meeting participant. May have constraints about when Items in
+    A meeting participant. May have constraints about when Meetings in
     which they are participants can be scheduled.
     """
 
@@ -59,18 +58,19 @@ class Participant:
 
 
 @dataclass
-class Item:
-    "A thing to be scheduled."
+class Meeting:
+    "A meeting to be scheduled."
 
     name: str
     participants: Set[Participant]
-    slots: Set[Slot]
+    slots: List[Slot]
 
     def copy(self):
         return dataclasses.replace(self)
 
     def remove_slots(self, fn: Predicate) -> bool:
-        new = {s for s in self.slots if not fn(s)}
+        "Remove slots indicated by the predicate and return True if anything changed."
+        new = [s for s in self.slots if not fn(s)]
         if len(new) < len(self.slots):
             self.slots = new
             return True
@@ -95,16 +95,16 @@ class Item:
     def unscheduled(self) -> bool:
         return len(self.slots) > 1
 
-    def overlapping_participants(self, other: "Item") -> bool:
+    def overlapping_participants(self, other: "Meeting") -> bool:
         return bool(self.participants & other.participants)
 
     def __str__(self):
         ps = [p.name for p in self.participants]
-        slot = list(self.slots)[0]
-        return f"{self.name} ({', '.join(sorted(ps))}): slot: {slot}"
+        slot = ",".join(str(s) for s in self.slots)
+        return f"{self.name} ({', '.join(sorted(ps))}): {slot}"
 
 
-def schedules(items: List[Item]) -> Iterator[List[Item]]:
+def schedules(items: List[Meeting]) -> Iterator[List[Meeting]]:
     "Iterator of filled in schedules."
 
     unscheduled = [i for i, item in enumerate(items) if item.unscheduled()]
@@ -118,12 +118,12 @@ def schedules(items: List[Item]) -> Iterator[List[Item]]:
         yield items
 
 
-def assign(items: List[Item], idx: int, s: Slot) -> Optional[List[Item]]:
+def assign(items: List[Meeting], idx: int, s: Slot) -> Optional[List[Meeting]]:
     "Assign the item at idx to Slot s and propagate the consequences."
     return eliminate([item.copy() for item in items], idx, lambda x: x != s)
 
 
-def eliminate(items: List[Item], idx: int, fn: Predicate) -> Optional[List[Item]]:
+def eliminate(items: List[Meeting], idx: int, fn: Predicate) -> Optional[List[Meeting]]:
     "Eliminate slots from item at idx and propagate consequences. Mutates the items."
 
     item = items[idx]
@@ -141,32 +141,3 @@ def eliminate(items: List[Item], idx: int, fn: Predicate) -> Optional[List[Item]
                         return None
 
     return items
-
-
-if __name__ == "__main__":
-
-    @dataclass(frozen=True)
-    class SimpleSlot(Slot):
-
-        name: str
-
-        def __str__(self):
-            return self.name
-
-    all_slots = {SimpleSlot(c) for c in "123"}
-
-    def item(s):
-        return Item(s, {Participant(c) for c in s}, set(all_slots))
-
-    items = [item(s) for s in ("abc", "def", "ace", "ghi", "cdg", "beh")]
-
-    i = 0
-    for s in schedules(items):
-        print(i)
-        for item in items:
-            print(item)
-        print()
-        i += 1
-
-    if i == 0:
-        print("no schedules!")
